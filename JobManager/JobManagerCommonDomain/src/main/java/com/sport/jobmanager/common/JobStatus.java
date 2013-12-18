b80484cd3cea0a0a2168e3d088c88900eec8d8da
Lukas Kubicek <lukas.kubicek@netcom-gsm.no>
2013-12-18 15:11:45 +0100

    init project commit

---
+++ JobManager/JobManagerCommonDomain/src/main/java/com/sport/jobmanager/common/JobStatus.java
@@ -0,0 +1,37 @@
+package com.sport.jobmanager.common;
+
+/**
+ *
+ * @author Lukas Kubicek 
+ */
+public enum JobStatus {
+
+    /**
+     * initial state ... ready to pick up
+     */
+    INITIAL,
+    /**
+     * job is processing
+     */
+    PROCESSING,
+    /**
+     * Job has been successfully completed
+     */
+    COMPLETED,
+    /**
+     * Job failed
+     */
+    FAILED;
+
+    public static JobStatus fromString(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        for (JobStatus status : values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        return null;
+    }
+}
